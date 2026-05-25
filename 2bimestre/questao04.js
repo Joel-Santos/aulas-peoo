5cb210d4fa2c175759c13622bac9587cf2a20f2a
Joel Santos <joeloliveira56@gmail.com>
2026-05-25 13:55:50 +0000

    feat: Aula JS PEOO Condicionais

---
+++ 2bimestre/questao04.js
@@ -0,0 +1,14 @@
+let opcao = 2;
+switch(opcao){
+    case 1: 
+        console.log("Novo Jogo");
+        break;
+    case 2:
+        console.log("Carregar jogo!");
+        break;
+    case 3:
+        console.log("Configurações");
+        break;
+    default:
+        console.log("Opção Inválida!");
+}
